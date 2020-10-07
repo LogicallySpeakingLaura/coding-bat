107dc78b1d84203f6943c2c336ab53bd750a8fef
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-07 16:09:58 -0600

    Create logic-1.py
    All problems solved with solutions.

---
+++ python/logic-1.py
@@ -0,0 +1,97 @@
+"""
+Author: LogicallySpeakingLaura
+Date last modified: 2020/10/07
+Basic boolean logic puzzles -- if else and or not.
+https://codingbat.com/python/Logic-1
+"""
+
+
+def cigar_party(cigars, is_weekend):
+    """
+    When squirrels get together for a party, they like to have cigars. A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. Unless it is the weekend, in which case there is no upper bound on the number of cigars. Return True if the party with the given values is successful, or False otherwise.
+    """
+    return (40 <= cigars <= 60 and not is_weekend) or (cigars >= 40 and is_weekend)
+
+
+def date_fashion(you, date):
+    """
+    You and your date are trying to get a table at a restaurant. The parameter "you" is the stylishness of your clothes, in the range 0..10, and "date" is the stylishness of your date's clothes. The result getting the table is encoded as an int value with 0=no, 1=maybe, 2=yes. If either of you is very stylish, 8 or more, then the result is 2 (yes). With the exception that if either of you has style of 2 or less, then the result is 0 (no). Otherwise the result is 1 (maybe).
+    """
+    table = 1 # assume maybe till style checked
+
+    if you >= 8 or date >= 8: # do not use if-else, both conditions need to be checked
+        table = 2
+
+    if you <= 2 or date <= 2:
+        table = 0
+
+    return table;
+
+
+def squirrel_play(temp, is_summer):
+    """
+    The squirrels in Palo Alto spend most of the day playing. In particular, they play if the temperature is between 60 and 90 (inclusive). Unless it is summer, then the upper limit is 100 instead of 90. Given an int temperature and a boolean is_summer, return True if the squirrels play and False otherwise.
+    """
+    return ( is_summer and 60 <= temp <= 100 ) or ( not is_summer and 60 <= temp <= 90 )
+
+
+def caught_speeding(speed, is_birthday):
+    """
+    You are driving a little too fast, and a police officer stops you. Write code to compute the result, encoded as an int value: 0=no ticket, 1=small ticket, 2=big ticket. If speed is 60 or less, the result is 0. If speed is between 61 and 80 inclusive, the result is 1. If speed is 81 or more, the result is 2. Unless it is your birthday -- on that day, your speed can be 5 higher in all cases.
+    """
+    ticket = 0
+
+    if is_birthday:
+        if 66 <= speed <= 85:
+            ticket = 1
+        elif speed > 85: # could do >= 86 if that logic is easier to understand
+            ticket = 2
+    else:
+        if 61 <= speed <= 80:
+            ticket = 1
+        elif speed > 80:
+            ticket = 2
+
+    return ticket
+
+
+def sorta_sum(a, b):
+    """
+    Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+    """
+    return 20 if 10 <= a + b < 20 else a + b # could do <= 19 if that logic is easier to understand
+
+
+def alarm_clock(day, vacation):
+    """
+    Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
+    """
+    alarm = '10:00';
+
+    if vacation and (day == 0 or day == 6): # is it a vacation and a weekend day
+        alarm = 'off'
+    elif not vacation and not (day == 0 or day == 6): # is it a weekday and no vacation
+        alarm = '7:00'
+
+    return alarm
+
+
+def love6(a, b):
+    """
+    The number 6 is a truly great number. Given two int values, a and b, return True if either one is 6. Or if their sum or difference is 6.
+    """
+    return a == 6 or b == 6 or a + b == 6 or abs( a - b ) == 6
+
+
+def in1to10(n, outside_mode):
+    """
+    Given a number n, return True if n is in the range 1..10, inclusive. Unless outside_mode is True, in which case return True if the number is less or equal to 1, or greater or equal to 10.
+    """
+    return ( outside_mode and ( n <= 1 or n >= 10 ) ) or (not outside_mode and 1 <= n <= 10)
+
+
+def near_ten(num):
+    """
+    Given a non-negative number "num", return True if num is within 2 of a multiple of 10.
+    """
+    return num % 10 <= 2 or num % 10 >= 8; # want result to equal 1, 2, 8 or 9
