1e28a8223d5243e08fa949fae79538d42ab8f161
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-08 00:56:58 -0600

    Update String2.java
    First problem solved.

--- java/String2.java
+++ java/String2.java
@@ -6,5 +6,19 @@
  */
 public class String2
 {
-    
+    /**
+     * Given a string, return a string where for every char in the original, there are two chars.
+     */
+    public String doubleChar( String str )
+    {
+        String rtrn = "";
+
+        for( int i = 0 ; i < str.length() ; i++ )
+        {
+            str += str.charAt(i);
+            str += str.charAt(i);
+        }
+
+        return rtrn;
+    }
 }
