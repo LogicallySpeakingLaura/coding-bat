63ae5a2864af26a8d3e8f7f221792feaf9eea256
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-08 21:38:49 -0600

    Update logic-1.py
    Simplified code.

--- python/logic-1.py
+++ python/logic-1.py
@@ -66,14 +66,8 @@ def alarm_clock(day, vacation):
     """
     Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean indicating if we are on vacation, return a string of the form "7:00" indicating when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on the weekend it should be "10:00". Unless we are on vacation -- then on weekdays it should be "10:00" and weekends it should be "off".
     """
-    alarm = '10:00';
-
-    if vacation and (day == 0 or day == 6): # is it a vacation and a weekend day
-        alarm = 'off'
-    elif not vacation and not (day == 0 or day == 6): # is it a weekday and no vacation
-        alarm = '7:00'
-
-    return alarm
+            # is it a vacation and a weekend day                is it a weekday and no vacation
+    return 'off' if vacation and (day == 0 or day == 6) else '7:00' if not vacation and not (day == 0 or day == 6) else '10:00'
 
 
 def love6(a, b):
