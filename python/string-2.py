6876226d50dec7e55f40351dbf3edc98e0f8ef4d
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-08 20:38:05 -0600

    Update string-2.py
    All problems solved with explanations.

--- python/string-2.py
+++ python/string-2.py
@@ -9,19 +9,39 @@ def double_char(str):
     """
     Given a string, return a string where for every char in the original, there are two chars.
     """
-    rtrn = ""
-
-    for i in range(len(str)):
-        rtrn += str[i : i + 1] * 2 # add two of the same char
-
-    return rtrn
+    return ''.join(char * 2 for char in str) # need to have something to join to so ''
 
 
 def count_hi(str):
-    count = 0
+    """
+    Return the number of times that the string "hi" appears anywhere in the given string.
+    """
+    return str.count('hi')
 
-    for i in range(len(str) - 1):
-        if str[i: i + 2] == 'hi':
-            count += 1
 
-    return count
+def cat_dog(str):
+    """
+    Return True if the string "cat" and "dog" appear the same number of times in the given string.
+    """
+    return str.count('cat') == str.count('dog')
+
+
+def count_code(str):
+    """
+    Return the number of times that the string "code" appears anywhere in the given string, except we'll accept any letter for the 'd', so "cope" and "cooe" count.
+    """
+    return sum(1 for i in range(len(str) - 3) if str[i:i + 2] == 'co' and str[i + 3] == 'e') # 1 increases every time condition met
+
+
+def end_other(a, b):
+    """
+    Given two strings, return True if either of the strings appears at the very end of the other string, ignoring upper/lower case differences (in other words, the computation should not be "case sensitive").
+    """
+    return b.lower().endswith( a.lower() ) or a.lower().endswith( b.lower() )
+
+
+def xyz_there(str):
+    """
+    Return True if the given string contains an appearance of "xyz" where the xyz is not directly preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+    """
+    return str.count('.xyz') != str.count('xyz') # as soon as a .xyz occurs or never occurs they don't evaluate the same
