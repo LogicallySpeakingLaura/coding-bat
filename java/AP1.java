5456eb8711edc7c9873c69e6d1d182bece1ada9a
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-20 16:40:17 -0600

    Create AP1.java

---
+++ java/AP1.java
@@ -0,0 +1,11 @@
+/**
+ * @author LogicallySpeakingLaura
+ * @version 2020/10/20
+ * AP CS medium problems. These problems are based on free response questions from old AP CS-A exams, 2004 and later. Some are from the exams and some were just created to have a similar difficulty. These problems are good practice for the logic/loop/array aspects of the exam.
+ * https://codingbat.com/java/AP-1
+ */
+public class AP1
+{
+
+    
+}
