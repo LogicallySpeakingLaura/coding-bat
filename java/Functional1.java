6e954734d3b1853fc975e4377c92846e3da1fbce
LogicallySpeakingLaura <60587339+LogicallySpeakingLaura@users.noreply.github.com>
2020-10-27 16:23:56 -0600

    Create Functional1.java

---
+++ java/Functional1.java
@@ -0,0 +1,10 @@
+/**
+ * @author LogicallySpeakingLaura
+ * @version 2020/10/27
+ * Functional mapping operations on lists with lambdas. The main computation for each of these problems can be done with 1 line of lambda code.
+ * https://codingbat.com/java/Functional-1
+ */
+public class Functional1
+{
+    
+}
